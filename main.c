62974a70242f4b0f5244ddbca0e0553c9ddfad85
Nikko Dela Cruz Ferwelo <31774519+ratburat69@users.noreply.github.com>
2020-03-28 14:18:29 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
-// C program to demonstrate insert operation in binary search tree.
+
 #include<stdio.h>
 #include<stdlib.h>
 
 struct node
 {
-    int key;
+    int data;
     struct node *left, *right;
 };
 
 struct node *newNode(int item)
 {
     struct node *temp = (struct node *)malloc(sizeof(struct node));
-    temp->key = item;
+    temp->data = item;
     temp->left = temp->right = NULL;
     return temp;
 }
@@ -22,22 +22,22 @@ void inorder(struct node *root)
     if (root != NULL)
     {
         inorder(root->left);
-        printf("%d \n", root->key);
+        printf("%d \n", root->data);
         inorder(root->right);
     }
 }
 
 
-struct node* insert(struct node* node, int key)
+struct node* insert(struct node* node, int data)
 {
 
-    if (node == NULL) return newNode(key);
+    if (node == NULL) return newNode(data);
     
   
-    if (key < node->key)
-        node->left = insert(node->left, key);
-    else if (key > node->key)
-        node->right = insert(node->right, key);
+    if (key < node->data)
+        node->left = insert(node->left, data);
+    else if (key > node->data)
+        node->right = insert(node->right, data);
     
 
     return node;
